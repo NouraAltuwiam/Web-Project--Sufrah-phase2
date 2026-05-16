4dd295abf949d04b40848fc6c3215b87c2c145fc
Haya Hamad <hhaayyaa1467@gmail.com>
2026-05-16 23:30:22 +0300

    Update database connection details in dp.php

--- dp.php
+++ dp.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'sql208.infinityfree.com';
-$db   = 'if0_41938939_sufrah';
-$user = 'if0_41938939';
-$pass = 'كلمة مرور الـ vPanel';
+$host = 'sql303.infinityfree.com';
+$db   = 'if0_41939924_sufrah';
+$user = 'if0_41939924';
+$pass = 'XADHs6Pes4tAiv';
 
 $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
